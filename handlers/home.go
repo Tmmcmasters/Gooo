a29b7ac23a36423bbb5b684deee1f8f8b61736af
tmmcmasters <tmmcmasters@gmail.com>
2025-07-04 12:24:39 -0400

    Removed the runtime file access

--- handlers/home.go
+++ handlers/home.go
@@ -3,20 +3,10 @@ package handlers
 import (
 	serverPages "PersonalPortfolio/components/server/pages"
 	"PersonalPortfolio/helpers"
-	"log"
-	"os"
 
 	"github.com/labstack/echo/v4"
 )
 
 func HomeHandler(context echo.Context) error  {
-	cssBytes, err := os.ReadFile("static/assets/css/output-tw.css");
-	if err != nil {
-		log.Printf("Failed to read output-tw.css: %v", err)
-		return helpers.Render(context, 500, serverPages.Home(""));
-	}
-
-	cssContent := string(cssBytes);
-
-	return helpers.Render(context, 200, serverPages.Home(cssContent))
+	return helpers.Render(context, 200, serverPages.Home())
 }
