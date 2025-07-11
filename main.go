ae57312d388fe16bc24c4b9411c4db3d375156f6
tmmcmasters <tmmcmasters@gmail.com>
2025-07-11 15:43:56 -0400

    Better Environment Log

--- main.go
+++ main.go
@@ -15,6 +15,22 @@ import (
 	"github.com/labstack/echo/v4/middleware"
 )
 
+const (
+	Red    = "\033[31m"
+	Green  = "\033[32m"
+	Yellow = "\033[33m"
+	Blue   = "\033[34m"
+	BgRed    = "\033[41m"
+	BgGreen  = "\033[42m"
+	BgYellow = "\033[43m"
+	BgBlue   = "\033[44m"
+	BgMagenta  = "\033[45m"
+	WhiteText  = "\033[97m"
+	Bold       = "\033[1m"
+	Underline  = "\033[4m"
+	Reset    = "\033[0m"
+)
+
 func main()  {
 	envFile := os.Getenv("ENV_FILE");
 
@@ -26,8 +42,9 @@ func main()  {
 		log.Printf("WARNING: Couldn't load %s file: %v", envFile, err);
 	}
 
-	log.Printf("Using environment: %s", os.Getenv("ENV"))
-
+	log.SetFlags(0)
+	log.Println("")
+	log.Printf("\u2022 Environment: %s%s%s%s", Blue, Bold, os.Getenv("ENV"), Reset)
 
 	e := echo.New();
 
@@ -141,5 +158,7 @@ func main()  {
 
 	e.Logger.Fatal(e.Start(":" + appPort));
 }
+
+
 
 
