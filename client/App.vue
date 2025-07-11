dff11677a20f22ee08206b500ce2d51124eb7819
tmmcmasters <tmmcmasters@gmail.com>
2025-07-11 14:20:35 -0400

    Basic Theme switcher

--- client/App.vue
+++ client/App.vue
@@ -1,6 +1,8 @@
 <script setup lang="ts">
 import { onBeforeMount, onMounted, ref, watch } from 'vue'
 import Button from './components/ui/button/Button.vue'
+import Switch from './components/ui/switch/Switch.vue'
+import { useColorMode } from '@vueuse/core'
 
 type Inputs = {
   input1: string
@@ -38,14 +40,20 @@ onMounted(() => {
   console.log('On mounted')
   stop()
 })
+
+const color = useColorMode({})
+
+const isDark = ref(color.value === 'dark')
+
+const changeColor = (value: boolean) => {
+  color.value = value ? 'dark' : 'light'
+  isDark.value = value
+}
 </script>
 
 <template>
   <div>
-    <div class="w-full h-full bg-base-100">
-      <button class="btn-accent btn">Here Here</button>
-      <input type="checkbox" value="synthwave" class="toggle theme-controller" />
-    </div>
+    <Switch :model-value="isDark" @update:model-value="changeColor($event)" />
     <form>
       <label for="input1">Input 1:</label>
       <input v-model="inputs.input1" id="input1" type="text" />
@@ -60,17 +68,10 @@ onMounted(() => {
     <p>Input 2: {{ inputs.input2 }}</p>
     <p>Input 3: {{ inputs.input3 }}</p>
 
-    <Button variant="ghost" @click="console.log('Here is a click')">Shadcn Button</Button>
+    <Button variant="outline" @click="console.log('Here is a click')">Shadcn Button</Button>
 
     <TestComponent />
   </div>
 </template>
 
-<style scoped>
-label {
-  margin-right: 10px;
-}
-input {
-  margin-bottom: 10px;
-}
-</style>
+<style scoped></style>
