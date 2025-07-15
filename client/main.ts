0d19aaf93d07cbe7cb5dc72b83ad6517edaf266d
tmmcmasters <tmmcmasters@gmail.com>
2025-07-15 09:37:05 -0400

    Moving some files around and making the client side more organized

--- client/main.ts
+++ client/main.ts
@@ -1,8 +1,8 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-import App from './App.vue'
+import Home from './page/Home.vue'
 
-const app = createApp(App)
+const app = createApp(Home)
 
 app.use(createPinia())
 
