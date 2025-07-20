00427938b14b910633fede587697a21944cab289
tmmcmasters <tmmcmasters@gmail.com>
2025-07-20 16:47:55 -0400

    Testing some stuff out

--- client/page/Home.vue
+++ client/page/Home.vue
@@ -61,7 +61,7 @@ const { increment } = useCounterStore()
         stroke-width="2"
         stroke-linecap="round"
         stroke-linejoin="round"
-        class="size-5 text-neutral-900"
+        class="size-5 text-foreground"
       >
         <path stroke="none" d="M0 0h24v24H0z" fill="none" />
         <path d="M12 12m-4 0a4 4 0 1 0 8 0a4 4 0 1 0 -8 0" />
@@ -78,7 +78,7 @@ const { increment } = useCounterStore()
         stroke-width="2"
         stroke-linecap="round"
         stroke-linejoin="round"
-        class="size-5 text-neutral-900"
+        class="size-5 text-foreground"
       >
         <path stroke="none" d="M0 0h24v24H0z" fill="none" />
         <path d="M12 3c.132 0 .263 0 .393 0a7.5 7.5 0 0 0 7.92 12.446a9 9 0 1 1 -8.313 -12.454z" />
