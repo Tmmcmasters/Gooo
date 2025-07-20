53342fade8b141a5c638934e7cf1b277bcdd6a94
tmmcmasters <tmmcmasters@gmail.com>
2025-07-20 17:33:18 -0400

    Created the base card for this starting.

--- client/page/Home.vue
+++ client/page/Home.vue
@@ -85,6 +85,50 @@ const { increment } = useCounterStore()
       </svg>
     </Button>
     <div class="flex items-center justify-between h-full gap-5 w-full">
+      <Button
+        variant="outline"
+        class="max-w-80 w-full h-32 text-2xl flex relative flex-col justify-start items-start gap-y-1.5"
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="currentColor"
+          stroke-width="1.5"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          class="size-8 p-0.5 text-foreground border border-border rounded-md"
+        >
+          <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+          <path d="M5 4m0 1a1 1 0 0 1 1 -1h2a1 1 0 0 1 1 1v14a1 1 0 0 1 -1 1h-2a1 1 0 0 1 -1 -1z" />
+          <path d="M9 4m0 1a1 1 0 0 1 1 -1h2a1 1 0 0 1 1 1v14a1 1 0 0 1 -1 1h-2a1 1 0 0 1 -1 -1z" />
+          <path d="M5 8h4" />
+          <path d="M9 16h4" />
+          <path
+            d="M13.803 4.56l2.184 -.53c.562 -.135 1.133 .19 1.282 .732l3.695 13.418a1.02 1.02 0 0 1 -.634 1.219l-.133 .041l-2.184 .53c-.562 .135 -1.133 -.19 -1.282 -.732l-3.695 -13.418a1.02 1.02 0 0 1 .634 -1.219l.133 -.041z"
+          />
+          <path d="M14 9l4 -1" />
+          <path d="M16 16l3.923 -.98" />
+        </svg>
+        <span class="font-semibold text-lg leading-none">Documentation</span>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="currentColor"
+          stroke-width="2"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          class="size-5 absolute top-3 right-3"
+        >
+          <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+          <path d="M17 7l-10 10" />
+          <path d="M8 7l9 0l0 9" />
+        </svg>
+        <span class="text-sm text-muted-foreground text-pretty text-left w-full"
+          >It is recommended to read the documentation to get started.</span
+        >
+      </Button>
       <Button @click="increment" variant="outline" class="max-w-80 w-full h-28 text-2xl"
         >Counter: {{ count }}
       </Button>
