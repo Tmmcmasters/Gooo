4a01cb7376dae943b520301b50cffedd6ae118d5
tmmcmasters <tmmcmasters@gmail.com>
2025-07-24 20:16:26 -0400

    Add more explanations of this goooHydrate

--- client/utils/goooHydrate.ts
+++ client/utils/goooHydrate.ts
@@ -17,6 +17,9 @@ declare global {
 
 /**
  * Necessary to hydrate the component and set page registration. This provides a way to dynamically load the component whenever navigating between routes and be able to prefetch when wanted.
+ * @param {string} genFilePath - The path of the generated file that will be used for hydration.
+ * @param {string} mountPoint - The selector for the element which the vue app will be mounted to.
+ * @param {function} hydrate - The function that will be called to hydrate the component.
  */
 export default (genFilePath: string, mountPoint: string, hydrate: () => void) => {
 
@@ -37,4 +40,3 @@ export default (genFilePath: string, mountPoint: string, hydrate: () => void) =>
 }
 
 window.pageRegistry = window.pageRegistry || new Map()
-
