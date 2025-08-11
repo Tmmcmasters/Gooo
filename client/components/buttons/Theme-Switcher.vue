7a662074a37bf71f9c14a1cd843d2de08d7a4739
tmmcmasters <tmmcmasters@gmail.com>
2025-08-11 16:19:11 -0400

    I have to figure out why the counter is not being shared to the theme switcher

--- client/components/buttons/Theme-Switcher.vue
+++ client/components/buttons/Theme-Switcher.vue
@@ -3,15 +3,21 @@ import useThemeCookie from '@/composables/useThemeCookie'
 import Button from '../ui/button/Button.vue'
 import SunIcon from '../icons/SunIcon.vue'
 import MoonIcon from '../icons/MoonIcon.vue'
+import { useCounterStore } from '@/stores/counter'
 
 const { isDark, changeColor } = useThemeCookie()
+
+const { count } = useCounterStore()
 </script>
 
 <template>
-  <Button @click="changeColor" variant="ghost" size="icon" class="absolute top-5 right-5">
-    <SunIcon v-if="isDark" class="size-5 text-foreground" />
-    <MoonIcon v-else class="size-5 text-foreground" />
-  </Button>
+  <div class="absolute top-5 right-5 flex justify-center items-center gap-x-5">
+    <p>Count: {{ count }}</p>
+    <Button @click="changeColor" variant="ghost" size="icon">
+      <SunIcon v-if="isDark" class="size-5 text-foreground" />
+      <MoonIcon v-else class="size-5 text-foreground" />
+    </Button>
+  </div>
 </template>
 
 <style scoped></style>
--- client/themeSwitcher.ts
+++ client/themeSwitcher.ts
@@ -1,9 +1,11 @@
 import { createApp } from "vue";
 import goooHydrate from "./utils/goooHydrate";
 import ThemeSwitcher from "./components/buttons/Theme-Switcher.vue";
+import { createPinia } from "pinia";
 
 
 goooHydrate('/gen/js/themeSwitcher.js', '#theme-switcher', () => {
     const app = createApp(ThemeSwitcher)
+    app.use(createPinia())
     app.mount('#theme-switcher')
 })
